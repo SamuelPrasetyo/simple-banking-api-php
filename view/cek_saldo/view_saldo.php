3b49c31736a1e0493ea43ff9ed0f42c0eddbb74d
Samuel Prasetyo <sprasetyo0511@gmail.com>
2023-09-17 14:55:50 +0700

    Membuat fitur cek saldo dengan API

--- view/cek_saldo/view_saldo.php
+++ view/cek_saldo/view_saldo.php
@@ -11,24 +11,55 @@
     <?php
         include '../include/header.php';
         include '../../koneksi.php';
-        $query = "SELECT * FROM rekening WHERE no_rekening=1809094352934";
-        $result = mysqli_query($conn, $query);
-    ?>
 
-    <h2 style="margin: 100px 0px 0px 120px;">Saldo Anda</h2>
-    <?php
-        while ($rec = mysqli_fetch_assoc($result)) {
-            echo '
+        // Fungsi untuk cek saldo berdasarkan nomor rekening
+        function cekSaldo($nomor_rekening) {
+            global $conn;
+
+            $query = "SELECT no_rekening, nama_nasabah, saldo FROM rekening WHERE no_rekening = ?";
+            $stmt = $conn->prepare($query);
+            $stmt->bind_param("s", $nomor_rekening);
+            $stmt->execute();
+            $result = $stmt->get_result();
+
+            if ($result->num_rows > 0) {
+                $row = $result->fetch_assoc();
+                return $row;
+            } else {
+                return null;
+            }
+        }
+
+        // Ambil nomor rekening dari permintaan HTTP
+        if (isset($_GET["nomor_rekening"])) {
+            $nomor_rekening = $_GET["nomor_rekening"];
+            $nasabah = cekSaldo($nomor_rekening);
+            
+            if ($nasabah !== null) {
+                $nomor_rekening = $nasabah["no_rekening"];
+                $nama = $nasabah["nama_nasabah"];
+                $saldo = $nasabah["saldo"];
+                echo '
+                <h2 style="margin: 100px 0px 0px 120px;">Saldo Anda</h2>
                 <div class="card" style="width: 18rem; margin: 10px 0px 0px 120px">
                     <div class="card-body">
-                        <h5 class="card-title">' . $rec['no_rekening'] . '</h5>
-                        <h6 class="card-subtitle mb-2 text-body-secondary">' . $rec['nama_nasabah'] . '</h6>
-                        <p class="card-text">Rp. ' . $rec['saldo'] . '</p>
+                        <h5 class="card-title">' . $nomor_rekening . '</h5>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">' . $nama . '</h6>
+                        <p class="card-text">Rp. ' . $saldo . '</p>
                     </div>                    
                 </div>
             ';
+            } else {
+                echo "Nomor rekening tidak ditemukan.";
+            }
+        } else {
+            echo "Masukkan nomor rekening untuk cek saldo.";
         }
     ?>
+
+
+
+    
 
     <!-- <div class="card" style="width: 18rem; margin: 400px 0px 0px 120px">
         <div class="card-body">
